7f005ced9e28a50cc4737ba1d621e7c533035a98
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-06-12 10:03:26 +0545

    Completed login backend

--- lib/screens/auth/SigninScreen.dart
+++ lib/screens/auth/SigninScreen.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:hirehub/screens/auth/DecorationFunction.dart';
+import 'package:hirehub/models/Users.dart';
+import 'package:hirehub/repository/UserRepository.dart';
 import 'package:hirehub/screens/auth/SigninUpBar.dart';
+import 'package:motion_toast/motion_toast.dart';
 
 import '../../config/Palette.dart';
 import 'Title.dart';
 
-class SigninScreen extends StatelessWidget {
+class SigninScreen extends StatefulWidget {
   final VoidCallback? onRegisterClicked;
 
   const SigninScreen({Key? key, required this.onRegisterClicked})
       : super(key: key);
+
+  @override
+  State<SigninScreen> createState() => _SigninScreenState();
+}
+
+class _SigninScreenState extends State<SigninScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  _loginUser(User user) async {
+    bool isLogin = await UserRepository().loginUser(user);
+    if (isLogin) {
+      MotionToast.success(
+        description: const Text("Login Success"),
+      ).show(context);
+    } else {
+      MotionToast.error(
+        description: const Text("Login Failed"),
+      ).show(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,44 +50,75 @@ class SigninScreen extends StatelessWidget {
           ),
           Expanded(
             flex: 4,
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: TextFormField(
-                    decoration: signInInputDecoration(hintText: "Email"),
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: TextFormField(
+                      controller: _usernameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter username';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: TextFormField(
-                    decoration: signInInputDecoration(hintText: "Password"),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please enter password';
+                        }
+                        return null;
+                      },
+                    ),
                   ),
-                ),
-                SignInBar(
-                    label: "Login",
-                    isLoading: false,
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/home");
-                    }),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: InkWell(
-                    splashColor: Colors.white,
-                    onTap: () {
-                      onRegisterClicked?.call();
-                    },
-                    child: const Text(
-                      'Sign up',
-                      style: TextStyle(
-                        fontSize: 16,
-                        decoration: TextDecoration.underline,
-                        color: Palette.darkBlue,
+                  SignInBar(
+                      label: "Login",
+                      isLoading: false,
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          User user = User(
+                            username: _usernameController.text,
+                            password: _passwordController.text,
+                          );
+                          print(user);
+                          _loginUser(user);
+                        }
+                      }),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: InkWell(
+                      splashColor: Colors.white,
+                      onTap: () {
+                        widget.onRegisterClicked?.call();
+                      },
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,
+                          color: Palette.darkBlue,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           )
         ],
