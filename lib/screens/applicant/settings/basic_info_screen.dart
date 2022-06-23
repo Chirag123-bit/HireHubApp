927b5c83c55661b58d366a477dd7ca6474281167
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-06-23 08:33:13 +0545

    Started working on basic profile settings

--- lib/screens/applicant/settings/basic_info_screen.dart
+++ lib/screens/applicant/settings/basic_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hirehub/theme/Theme.dart';
 
 class EditBasicInfoScreen extends StatefulWidget {
@@ -26,14 +27,70 @@ class _EditBasicInfoScreenState extends State<EditBasicInfoScreen> {
         ],
       ),
       body: Container(
+          padding: const EdgeInsets.only(left: 16, top: 25, right: 16),
           child: ListView(
-        children: [
-          Text(
-            "Edit Profile",
-            style: titleStyle,
-          )
-        ],
-      )),
+            children: [
+              Text(
+                "Edit Profile",
+                style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Stack(children: [
+                  Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            color: Colors.black.withOpacity(0.1),
+                            offset: const Offset(0, 10)),
+                      ],
+                      image: DecorationImage(
+                          image: Image.asset("assets/images/profile.jpg").image,
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: primaryClr,
+                        border: Border.all(
+                          width: 4,
+                          color: Theme.of(context).scaffoldBackgroundColor,
+                        ),
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          )),
     );
   }
 }
