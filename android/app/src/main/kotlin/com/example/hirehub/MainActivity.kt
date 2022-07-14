62e81795ac60d6d58b0ee21d7c4df01f532cd18b
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-07-14 23:23:42 +0545

    Saftey 2

--- android/app/src/main/kotlin/com/example/hirehub/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hirehub/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.hirehub
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+   override fun configureFlutterEngine(@NonNull flutterEngine: 
+     FlutterEngine) {
+     GeneratedPluginRegistrant.registerWith(flutterEngine)
+   }
 }
