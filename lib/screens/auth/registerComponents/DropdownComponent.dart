85b5d7617fc4fe291fe2730166ddc13ac8454115
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-07-17 21:36:53 +0545

    register ui change

--- lib/screens/auth/registerComponents/DropdownComponent.dart
+++ lib/screens/auth/registerComponents/DropdownComponent.dart
@@ -22,24 +22,29 @@ class _DropdownComponentState extends State<DropdownComponent> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8),
-      child: DropdownButtonFormField(
-        hint: const Text('Select your Choice'),
-        items: widget.items,
-        isExpanded: true,
-        value: widget.value ?? widget.valueHolder,
-        onChanged: (value) {
-          widget.onChanged(value.toString());
-        },
-        validator: (value) {
-          if (value == null) {
-            return 'Please select an option';
-          }
-          return null;
-        },
-        decoration: InputDecoration(
-          border: const OutlineInputBorder(),
-          hintText: 'Select your Type',
-          hintStyle: TextStyle(color: Colors.grey[400]),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.white,
+        ),
+        child: DropdownButtonFormField(
+          hint: const Text('Select your Choice'),
+          items: widget.items,
+          isExpanded: true,
+          value: widget.value,
+          onChanged: (value) {
+            widget.onChanged(value.toString());
+          },
+          validator: (value) {
+            if (value == null) {
+              return 'Please select an option';
+            }
+            return null;
+          },
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            hintText: 'Select your Type',
+            hintStyle: TextStyle(color: Colors.grey[400]),
+          ),
         ),
       ),
     );
